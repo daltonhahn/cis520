ca81463f6553d50ba7434971bf91c96f10fe7c32
Garrett Blehm <gblehm29@gmail.com>
2018-02-12 09:51:31 -0600

    Changes for sema_up priority

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -112,12 +112,25 @@ sema_up (struct semaphore *sema)
 
   ASSERT (sema != NULL);
 
+
+
+  struct thread *t;
+  // struct thread *cur;
+  /* checks if current thread should yield */
+
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  {
+    t = list_entry (list_pop_front (&sema->waiters), struct thread, elem);
+    thread_unblock (t);
+    //cur = thread_current();
+  }
   sema->value++;
-  intr_set_level (old_level);
+
+  thread_try_preempt();
+
+  intr_set_level(old_level);
+
 }
 
 static void sema_test_helper (void *sema_);
--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -70,6 +70,8 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+bool priority_great (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED);
+void thread_try_preempt(void);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
@@ -237,9 +239,10 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, &priority_great, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
+
 }
 
 /* Returns the name of the running thread. */
@@ -308,7 +311,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, &priority_great, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -582,3 +585,28 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+/* Function to compare priorities and get the greatest value */
+bool
+priority_great (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED) 
+{
+  const struct thread *a = list_entry (a_, struct thread, elem);
+  const struct thread *b = list_entry (b_, struct thread, elem);
+
+  return a->priority > b->priority;
+}
+
+void 
+thread_try_preempt(void)
+{
+  struct thread *cur = thread_current();
+  //struct thread *next = ;
+
+  if (!list_empty(&ready_list) && (cur->priority < list_entry(list_front(&ready_list), struct thread, elem)->priority))
+  {
+    if(!intr_context()){
+      thread_yield();
+    }
+    else intr_yield_on_return();
+  }
+}
