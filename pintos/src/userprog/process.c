408c762c47543175706de42b46dc1a8740f32a95
Dalton Hahn <hahnd11@viper.cs.ksu.edu>
2018-03-04 18:37:47 -0600

    Fixing global var

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -18,7 +18,7 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-#define INITIAL_USER_PAGE 0x08048000 |- PGSIZE
+#define INITIAL_USER_PAGE 0x08048000 - PGSIZE
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
